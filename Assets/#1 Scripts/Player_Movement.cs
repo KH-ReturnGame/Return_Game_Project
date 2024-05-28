249834b1011505133b5e8695b4d0aa16b1f3193c
Monakai0 <164456230+Monakai0@users.noreply.github.com>
2024-05-29 00:19:58 +0900

    점프 구현 ( 무한 점프 방지 필요 )

--- Assets/#1 Scripts/Player_Movement.cs
+++ Assets/#1 Scripts/Player_Movement.cs
@@ -14,7 +14,14 @@ public class Player_Movement : MonoBehaviour
     private float _movementInputDirection;
     private float _movementSpeed = 10.00f;
     private Player _player;
-    
+    public float _jumpForce = 5f;
+    /*
+     - 무한 점프 방지 관련 코드
+    public LayerMask groundLayer;
+    public float groundCheckDistance = 0.1f;
+    private bool isGrounded;
+    */
+
     //제일 처음 호출
     void Start()
     {
@@ -25,8 +32,23 @@ public class Player_Movement : MonoBehaviour
     //매 프레임 실행
     void Update()
     {
+        //바닥 체크하기
+        // isGrounded = Physics.Raycast(transform.position, Vector3.down, groundCheckDistance, platform);
+
         //입력 체크하기
         CheckInput();
+
+        // 점프 실행
+        if ( Input.GetButtonDown("Jump"))
+        {
+            Jump();
+        }
+        /*
+        if (isGrounded && Input.GetButtonDown("Jump"))
+        {
+            Jump();
+        }
+        */
     }
 
     //0.02초마다 실행
@@ -44,5 +66,10 @@ public class Player_Movement : MonoBehaviour
     private void ApplyMovement()
     {
         _playerRigidbody.velocity = new Vector2(_movementInputDirection * _movementSpeed, _playerRigidbody.velocity.y);
+    }
+
+    private void Jump()
+    {
+        _playerRigidbody.AddForce(Vector3.up * _jumpForce, ForceMode2D.Impulse);
     }
 }
