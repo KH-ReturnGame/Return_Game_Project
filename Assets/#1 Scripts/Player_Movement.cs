b1bca10a174beaca830f9cfd1d1b4905f2209504
Monakai0 <164456230+Monakai0@users.noreply.github.com>
2024-05-29 02:33:17 +0900

    대시 기능 주석 추가

--- Assets/#1 Scripts/Player_Movement.cs
+++ Assets/#1 Scripts/Player_Movement.cs
@@ -14,9 +14,8 @@ public class Player_Movement : MonoBehaviour
     private float _movementInputDirection;
     private float _movementSpeed = 10.00f;
     private Player _player;
-    public float _jumpForce = 5f;
 
-
+    public float _jumpForce = 5f;       // 점프 힘 관리     
     /*
      - 무한 점프 방지 관련 변수
     public LayerMask groundLayer;
@@ -24,11 +23,11 @@ public class Player_Movement : MonoBehaviour
     private bool isGrounded;
     */
 
-    private bool _canDash = true;
-    private bool _isDashing;
-    private float _dashPower = 24f;
-    private float _dashTime = 0.2f;
-    private float _dashCooldown = 1f;
+    private bool _canDash = true;       // 대시 가능 상태
+    private bool _isDashing;            // 대시 중 상태
+    private float _dashPower = 24f;     // 대시 힘 관리
+    private float _dashTime = 0.2f;     // 대시 작동 시간 
+    private float _dashCooldown = 1f;   // 대시 쿨타임
 
     [SerializeField] private TrailRenderer tr;
 
@@ -55,7 +54,7 @@ public class Player_Movement : MonoBehaviour
             Jump();
         }
         /*
-         -바닥 체크가 가능해지면 사용하는 코드
+         //바닥 체크가 가능해지면 사용하는 코드
         if (isGrounded && Input.GetButtonDown("Jump"))
         {
             Jump();
@@ -71,6 +70,7 @@ public class Player_Movement : MonoBehaviour
         {
             StartCoroutine(Dash());
         }
+
     }
 
     //0.02초마다 실행
@@ -105,13 +105,13 @@ public class Player_Movement : MonoBehaviour
     {
         _canDash = false;
         _isDashing = true;
-        float originalGravity = _playerRigidbody.gravityScale;
-        _playerRigidbody.gravityScale = 0f;
-        _playerRigidbody.velocity = new Vector2(transform.localScale.x * _dashPower, 0f);
-        tr.emitting = true;
+        float originalGravity = _playerRigidbody.gravityScale;      // 플레이어 원래 중력 저장
+        _playerRigidbody.gravityScale = 0f;     // 중력 0으로 바꿔 대시중에 영향 없게 설정 
+        _playerRigidbody.velocity = new Vector2(transform.localScale.x * _dashPower, 0f);       // 대시 적용
+        tr.emitting = true;     // 이펙트 적용
         yield return new WaitForSeconds(_dashTime);
         tr.emitting = false;
-        _playerRigidbody.gravityScale = originalGravity;
+        _playerRigidbody.gravityScale = originalGravity;    // 중력 값 되돌림
         _isDashing = false;
         yield return new WaitForSeconds(_dashCooldown);
         _canDash = true;
