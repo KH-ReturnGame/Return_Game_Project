fff8d47de7412ad855cb2130c2ba0af40c2cea2b
DNSK <sslgh1024@naver.com>
2024-06-08 22:18:46 +0900

    일단 플레이어 무브먼트 일단 정리함

--- Assets/#1 Scripts/Player/Player_Movement.cs
+++ Assets/#1 Scripts/Player/Player_Movement.cs
@@ -8,23 +8,26 @@ using System;
 /// <returns></returns>
 public class Player_Movement : MonoBehaviour
 {
+    //플레이어
     private Rigidbody2D _playerRigidbody;
-    private float _movementInputDirection;
+    private Player _player;
+    SpriteRenderer spriteRenderer;
+    
+    //기본 좌우, 점프
     private float _recentDirection = 1;
     [SerializeField]
     private float _movementSpeed = 10.00f;
-    private Player _player;
-    SpriteRenderer spriteRenderer;
-
-    public float _jumpForce = 5f;    
+    private float _movementInputDirection;
+    public float _jumpForce = 15f;    
     
+    //대시
     private float _dashPower = 24f;     // 대시 힘 관리
     private float _dashTime = 0.2f;     // 대시 작동 시간 
     private float _dashCooldown = 1f;   // 대시 쿨타임
     private float originalGravity;      // 플레이어 원래 중력
-
     [SerializeField] private TrailRenderer tr;
 
+    //검 애니메이션
     private Animator _animator;
     public GameObject sword;
 
@@ -40,22 +43,30 @@ public class Player_Movement : MonoBehaviour
     void Start()
     {
         _playerRigidbody = GetComponent<Rigidbody2D>();
-        spriteRenderer = GetComponent<SpriteRenderer>();
         _player = GetComponent<Player>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
         _player.AddState(PlayerStates.CanDash);
         originalGravity = _playerRigidbody.gravityScale;
-
         _animator = sword.GetComponent<Animator>();
     }
 
     //매 프레임 실행
     void Update()
     {
-        //입력 체크하기
-        CheckInput();
+        //가로 입력 체크하기 ----------------------------------------
+        _movementInputDirection = Input.GetAxisRaw("Horizontal");
+        if (_movementInputDirection != 0)
+        {
+            _recentDirection = _movementInputDirection;
+        }
+        // 방향전환
+        if (_recentDirection != 0)
+        {
+            spriteRenderer.flipX = _recentDirection == 1;
+        }
 
 
-        //바닥 체크가 가능해지면 사용하는 코드
+        //점프 코드 ----------------------------------------
         if (_player.IsContainState(PlayerStates.IsDashing))
         {
             if ( _player.IsContainState(PlayerStates.IsGround) && 
@@ -73,54 +84,42 @@ public class Player_Movement : MonoBehaviour
                 Jump();
             }
         }
-
-        // 벽타기 로직
-        if (_player._stateManager._currentState.Contains(_player._states[(int)PlayerStates.IsWall]) && !_player._stateManager._currentState.Contains(_player._states[(int)PlayerStates.IsGround]) && _movementInputDirection != 0)
+        
+        
+        // 대시 실행 ----------------------------------------
+        if (!_player.IsContainState(PlayerStates.IsDashing) && 
+            Input.GetKeyDown(KeyCode.LeftShift) && 
+            _player.IsContainState(PlayerStates.CanDash)
+           )
         {
-            WallSlide();
+            StartCoroutine(Dash());
+        }
+        
+
+        // 벽슬라이드 로직 ----------------------------------------
+        if (_player.IsContainState(PlayerStates.IsWall) && !_player.IsContainState(PlayerStates.IsGround) && _movementInputDirection != 0)
+        {
+            _playerRigidbody.velocity = new Vector2(_playerRigidbody.velocity.x, -wallSlideSpeed);
         }
         // 벽타기 취소
         else _player.RemoveState(PlayerStates.IsWall);
         
-
-        // 대시 실행
-        if (!_player.IsContainState(PlayerStates.IsDashing) && 
-            Input.GetKeyDown(KeyCode.LeftShift) && 
-            _player.IsContainState(PlayerStates.CanDash)
-            )
-        {
-            StartCoroutine(Dash());
-        }
-
-        // 방향전환
-        if (_recentDirection != 0)
-        {
-            spriteRenderer.flipX = _recentDirection == 1;
-        }
         
-        //칼 애니메이션 테스트
+        //칼 애니메이션 테스트 ----------------------------------------
         if (Input.GetKeyDown(KeyCode.Semicolon))
         {
             _animator.SetBool("open_sword",!_animator.GetBool("open_sword"));
         }
     }
 
-    //0.02초마다 실행
+    
+    //0.02초마다 실행  ----------------------------------------
     private void FixedUpdate()
     {
         //움직임 적용
         if (!_player.IsContainState(PlayerStates.IsDashing))
         {
             ApplyMovement();
-        }
-    }
-
-    private void CheckInput()
-    {
-        _movementInputDirection = Input.GetAxisRaw("Horizontal");
-        if (_movementInputDirection != 0)
-        {
-            _recentDirection = _movementInputDirection;
         }
     }
 
@@ -151,12 +150,5 @@ public class Player_Movement : MonoBehaviour
         _player.RemoveState(PlayerStates.IsDashing);
         yield return new WaitForSeconds(_dashCooldown);
         _player.AddState(PlayerStates.CanDash);
-    }
-
-
-
-    private void WallSlide()
-    {
-        _playerRigidbody.velocity = new Vector2(_playerRigidbody.velocity.x, -wallSlideSpeed);
     }
 }
