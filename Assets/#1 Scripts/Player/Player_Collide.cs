0c61c546bfde1c012eb21de2b8e222d64e4cd4f2
최성윤 <yeongjibro@gmail.com>
2024-07-12 13:26:03 +0900

    wall위에서 점프가능하도록 변경

--- Assets/#1 Scripts/Player/Player_Collide.cs
+++ Assets/#1 Scripts/Player/Player_Collide.cs
@@ -18,7 +18,7 @@ public class Player_Collide : MonoBehaviour
     public void OnTriggerEnter2D(Collider2D other)
     {
         //Debug.Log("바닥 닿음");
-        if (other.CompareTag("ground")&&name == "ground_check") 
+        if (other.CompareTag("ground")&&name == "ground_check"||other.CompareTag("wall")&&name == "ground_check") 
         {
             _player.AddState(PlayerStates.IsGround);
             Debug.Log("바닥 닿음");
@@ -34,7 +34,7 @@ public class Player_Collide : MonoBehaviour
     public void OnTriggerExit2D(Collider2D other)
     {
         //Debug.Log("바닥 떨어짐");
-        if (other.CompareTag("ground")&&name == "ground_check")
+        if (other.CompareTag("ground")&&name == "ground_check"||other.CompareTag("wall")&&name == "ground_check")
         {   
             Debug.Log("바닥 떨어짐");
             _player.RemoveState(PlayerStates.IsGround);    
