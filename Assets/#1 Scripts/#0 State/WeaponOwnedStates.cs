8aa8872dca320c85d348423da8a67b7e840ff29f
DNSK <sslgh1024@naver.com>
2024-07-15 10:33:58 +0900

    유니티 파일이 안올라갔네

---
+++ Assets/#1 Scripts/#0 State/WeaponOwnedStates.cs
@@ -0,0 +1,54 @@
+namespace WeaponOwnedStates
+{
+    public class CanDecrease : State<Weapon_Manager>
+    {
+        public override void Enter(Weapon_Manager weapon)
+        {
+            
+        }
+
+        public override void Execute(Weapon_Manager weapon)
+        {
+            
+        }
+        public override void Exit(Weapon_Manager weapon)
+        {
+            
+        }
+    }
+    public class IsReloadingRifle : State<Weapon_Manager>
+    {
+        public override void Enter(Weapon_Manager weapon)
+        {
+            //장전을 하는 동안에는 과열 감소 가능하게
+            weapon.AddState(WeaponStates.CanDecrease);
+        }
+
+        public override void Execute(Weapon_Manager weapon)
+        {
+            
+        }
+        public override void Exit(Weapon_Manager weapon)
+        {
+            
+        }
+    }
+    public class IsShootingRifle : State<Weapon_Manager>
+    {
+        public override void Enter(Weapon_Manager weapon)
+        {
+            //소총 발사하는 동안에는 감소 못하게
+            weapon.RemoveState(WeaponStates.CanDecrease);
+        }
+
+        public override void Execute(Weapon_Manager weapon)
+        {
+            
+        }
+        public override void Exit(Weapon_Manager weapon)
+        {
+            //소총 발사 끝나면 감소 가능하게
+            weapon.AddState(WeaponStates.CanDecrease);
+        }
+    }
+}
