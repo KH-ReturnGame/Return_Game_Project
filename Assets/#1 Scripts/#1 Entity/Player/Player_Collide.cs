0e57ef1b6562b271adecc0322a58074684371bb5
DNSK <sslgh1024@naver.com>
2024-07-16 10:44:08 +0900

    플레이어 콜라이더 문제랑 스프라이트 문제, 점프 문제 해결함

--- Assets/#1 Scripts/#1 Entity/Player/Player_Collide.cs
+++ Assets/#1 Scripts/#1 Entity/Player/Player_Collide.cs
@@ -18,7 +18,7 @@ public class Player_Collide : MonoBehaviour
     public void OnTriggerEnter2D(Collider2D other)
     {
         //Debug.Log("바닥 닿음");
-        if (other.CompareTag("ground")&&name == "ground_check"||other.CompareTag("wall")&&name == "ground_check") 
+        if (other.CompareTag("ground")&&name == "ground_check") 
         {
             _player.AddState(PlayerStates.IsGround);
             Debug.Log("바닥 닿음");
@@ -33,8 +33,8 @@ public class Player_Collide : MonoBehaviour
 
     public void OnTriggerExit2D(Collider2D other)
     {
-        //Debug.Log("바닥 떨어짐");
-        if (other.CompareTag("ground")&&name == "ground_check"||other.CompareTag("wall")&&name == "ground_check")
+        //땅에서 떨어졌다고 땅감지 콜라이더가 체크하면 땅감지 해제
+        if (other.CompareTag("ground")&&name == "ground_check")
         {   
             Debug.Log("바닥 떨어짐");
             _player.RemoveState(PlayerStates.IsGround);    
